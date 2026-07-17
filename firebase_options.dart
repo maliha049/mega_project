4443ab132de5cca508eff09e230e68c82b1721d8
Most. Maliha Akter <148312612+maliha049@users.noreply.github.com>
2026-07-17 18:55:00 +0600

    Add files via upload

---
+++ firebase_options.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions {
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyDummyKeyXXXXXXXXXXXXXXXXX',
+    appId: '1:878381338324:android:243d01300f95bd17e7b541',
+    messagingSenderId: '878381338324',
+    projectId: 'mega-b29c0',
+  );
+}
